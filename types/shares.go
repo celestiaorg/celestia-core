9d0e17849ef75059f35d14a350fb19b0e96ae09a
Ismail Khoffi <Ismail.Khoffi@gmail.com>
2020-09-16 09:49:40 +0200

    Test with Evidence and fix bug discovered by test

--- types/shares.go
+++ types/shares.go
@@ -3,6 +3,8 @@ package types
 import (
 	"bytes"
 
+	"github.com/gogo/protobuf/proto"
+	"github.com/lazyledger/lazyledger-core/libs/protoio"
 	"github.com/lazyledger/nmt/namespace"
 )
 
@@ -47,14 +49,24 @@ func (ns NamespacedShares) Shares() []Share {
 	return res
 }
 
-type Byter interface {
-	Bytes() []byte
-}
-
-func MakeShares(data []Byter, shareSize int, nidFunc func(elem interface{}) namespace.ID) NamespacedShares {
+// XXX: We could easily generalize this s.t. it does not use
+// proto.Message in the method signature. But for now this
+// does not seem necessary.
+func MakeShares(data []proto.Message, shareSize int, nidFunc func(elem interface{}) namespace.ID) NamespacedShares {
 	shares := make([]NamespacedShare, 0)
 	for _, element := range data {
-		rawData := element.Bytes() // data without nid
+		// TODO: implement a helper that also returns the size
+		// to make it possible to (cleanly) implement:
+		// https://github.com/lazyledger/lazyledger-specs/issues/69
+		// For now, we do not squeeze multiple Tx into one share and
+		// hence can ignore prefixing with the starting location of the
+		// first start of a transaction in the share (aka SHARE_RESERVED_BYTES)
+		rawData, err := protoio.MarshalDelimited(element)
+		if err != nil {
+			// The (abci) app has to guarantee that it only includes messages
+			// that can be encoded without an error
+			panic("Can not encode ")
+		}
 		nid := nidFunc(element)
 		if len(rawData) < shareSize {
 			rawShare := rawData
@@ -62,17 +74,24 @@ func MakeShares(data []Byter, shareSize int, nidFunc func(elem interface{}) name
 			share := NamespacedShare{paddedShare, nid}
 			shares = append(shares, share)
 		} else { // len(rawData) >= shareWithoutNidSize:
-			firstRawShare := rawData[:shareSize-1]
+			firstRawShare := rawData[:shareSize]
 			shares = append(shares, NamespacedShare{firstRawShare, nid})
-			rawData := rawData[:shareSize-1]
+			rawData := rawData[shareSize:]
 			for len(rawData) > 0 {
-				paddedShare := zeroPadIfNecessary(rawData[:shareSize-1], shareSize)
+				shareSizeOrLen := min(shareSize, len(rawData))
+				paddedShare := zeroPadIfNecessary(rawData[:shareSizeOrLen], shareSize)
 				share := NamespacedShare{paddedShare, nid}
 				shares = append(shares, share)
+				rawData = rawData[shareSizeOrLen:]
 			}
 		}
 	}
 	return shares
+}
+
+func min(a, b int) int {
+	if a <= b { return a}
+	return b
 }
 
 func zeroPadIfNecessary(share []byte, width int) []byte {
--- types/shares_test.go
+++ types/shares_test.go
@@ -1,9 +1,66 @@
 package types
 
 import (
+	"bytes"
 	"reflect"
 	"testing"
+
+	"github.com/gogo/protobuf/proto"
+	"github.com/lazyledger/lazyledger-core/libs/protoio"
+	"github.com/lazyledger/nmt/namespace"
 )
+
+func TestMakeShares(t *testing.T) {
+	// resveredTxNamespaceID := append(bytes.Repeat([]byte{0}, 7), 1)
+	// resveredIntermediateStateRootsNamespaceID := append(bytes.Repeat([]byte{0}, 7), 2)
+	val := NewMockPV()
+	blockID := makeBlockID([]byte("blockhash"), 1000, []byte("partshash"))
+	blockID2 := makeBlockID([]byte("blockhash2"), 1000, []byte("partshash"))
+	vote1 := makeVote(t, val, "chainID", 0, 10, 2, 1, blockID, defaultVoteTime)
+	vote2 := makeVote(t, val, "chainID", 0, 10, 2, 1, blockID2, defaultVoteTime)
+	testEvidence := &DuplicateVoteEvidence{
+		VoteA:     vote1,
+		VoteB:     vote2,
+		Timestamp: defaultVoteTime,
+	}
+	testEvidenceBytes, err := protoio.MarshalDelimited(testEvidence.ToProto())
+	if err != nil {
+		t.Fatalf("Could not encode evidence: %v, error: %v", testEvidence, err)
+	}
+	resveredEvidenceNamespaceID := append(bytes.Repeat([]byte{0}, 7), 3)
+	type args struct {
+		data      []proto.Message
+		shareSize int
+		nidFunc   func(elem interface{}) namespace.ID
+	}
+	tests := []struct {
+		name string
+		args args
+		want NamespacedShares
+	}{
+		{"evidence", args{
+			data:      []proto.Message{testEvidence.ToProto()},
+			shareSize: ShareSize,
+			nidFunc: func(elem interface{}) namespace.ID {
+				return resveredEvidenceNamespaceID
+			},
+		}, NamespacedShares{NamespacedShare{
+			Share: testEvidenceBytes[:ShareSize],
+			ID:    resveredEvidenceNamespaceID,
+		}, NamespacedShare{
+			Share: zeroPadIfNecessary(testEvidenceBytes[ShareSize:], ShareSize),
+			ID:    resveredEvidenceNamespaceID,
+		}}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := MakeShares(tt.args.data, tt.args.shareSize, tt.args.nidFunc); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("len(MakeShares()) = %v want %v", len(got), len(tt.want))
+				t.Errorf("MakeShares() = %v\n want %v", got, tt.want)
+			}
+		})
+	}
+}
 
 func Test_zeroPadIfNecessary(t *testing.T) {
 	type args struct {
@@ -15,11 +72,11 @@ func Test_zeroPadIfNecessary(t *testing.T) {
 		args args
 		want []byte
 	}{
-		{ "pad", args{[]byte{1,2,3},  6}, []byte{1,2,3,0,0,0}},
-		{ "not necessary (equal to shareSize)", args{[]byte{1,2,3},  3}, []byte{1,2,3}},
-		{ "not necessary (greater shareSize)", args{[]byte{1,2,3},  2}, []byte{1,2,3}},
+		{"pad", args{[]byte{1, 2, 3}, 6}, []byte{1, 2, 3, 0, 0, 0}},
+		{"not necessary (equal to shareSize)", args{[]byte{1, 2, 3}, 3}, []byte{1, 2, 3}},
+		{"not necessary (greater shareSize)", args{[]byte{1, 2, 3}, 2}, []byte{1, 2, 3}},
 	}
-		for _, tt := range tests {
+	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := zeroPadIfNecessary(tt.args.share, tt.args.width); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("zeroPadIfNecessary() = %v, want %v", got, tt.want)
