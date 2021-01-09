2e8befb57bf42eb2e8240a78ae715be5c6d9454e
Marko Baricevic <marbar3778@yahoo.com>
2021-01-09 20:23:34 +0100

    remove iavl

--- light/rpc/client_test.go
+++ light/rpc/client_test.go
@@ -1,90 +1,76 @@
 package rpc
 
 import (
-	"context"
-	"encoding/hex"
 	"fmt"
-	"testing"
 
 	ics23 "github.com/confio/ics23/go"
-	"github.com/cosmos/iavl"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/mock"
-	"github.com/stretchr/testify/require"
-	dbm "github.com/tendermint/tm-db"
 
-	abci "github.com/lazyledger/lazyledger-core/abci/types"
 	"github.com/lazyledger/lazyledger-core/crypto/merkle"
-	"github.com/lazyledger/lazyledger-core/libs/bytes"
-	lcmock "github.com/lazyledger/lazyledger-core/light/rpc/mocks"
 	tmcrypto "github.com/lazyledger/lazyledger-core/proto/tendermint/crypto"
-	rpcmock "github.com/lazyledger/lazyledger-core/rpc/client/mocks"
-	ctypes "github.com/lazyledger/lazyledger-core/rpc/core/types"
-	"github.com/lazyledger/lazyledger-core/types"
 )
 
 // TestABCIQuery tests ABCIQuery requests and verifies proofs. HAPPY PATH 😀
-func TestABCIQuery(t *testing.T) {
-	tree, err := iavl.NewMutableTree(dbm.NewMemDB(), 100)
-	require.NoError(t, err)
+// func TestABCIQuery(t *testing.T) {
+// 	tree, err := iavl.NewMutableTree(dbm.NewMemDB(), 100)
+// 	require.NoError(t, err)
 
-	var (
-		key   = []byte("foo")
-		value = []byte("bar")
-	)
-	tree.Set(key, value)
+// 	var (
+// 		key   = []byte("foo")
+// 		value = []byte("bar")
+// 	)
+// 	tree.Set(key, value)
 
-	commitmentProof, err := tree.GetMembershipProof(key)
-	require.NoError(t, err)
+// 	commitmentProof, err := tree.GetMembershipProof(key)
+// 	require.NoError(t, err)
 
-	op := &testOp{
-		Spec:  ics23.IavlSpec,
-		Key:   key,
-		Proof: commitmentProof,
-	}
+// 	op := &testOp{
+// 		Spec:  ics23.IavlSpec,
+// 		Key:   key,
+// 		Proof: commitmentProof,
+// 	}
 
-	next := &rpcmock.Client{}
-	next.On(
-		"ABCIQueryWithOptions",
-		context.Background(),
-		mock.AnythingOfType("string"),
-		bytes.HexBytes(key),
-		mock.AnythingOfType("client.ABCIQueryOptions"),
-	).Return(&ctypes.ResultABCIQuery{
-		Response: abci.ResponseQuery{
-			Code:   0,
-			Key:    key,
-			Value:  value,
-			Height: 1,
-			ProofOps: &tmcrypto.ProofOps{
-				Ops: []tmcrypto.ProofOp{op.ProofOp()},
-			},
-		},
-	}, nil)
+// 	next := &rpcmock.Client{}
+// 	next.On(
+// 		"ABCIQueryWithOptions",
+// 		context.Background(),
+// 		mock.AnythingOfType("string"),
+// 		bytes.HexBytes(key),
+// 		mock.AnythingOfType("client.ABCIQueryOptions"),
+// 	).Return(&ctypes.ResultABCIQuery{
+// 		Response: abci.ResponseQuery{
+// 			Code:   0,
+// 			Key:    key,
+// 			Value:  value,
+// 			Height: 1,
+// 			ProofOps: &tmcrypto.ProofOps{
+// 				Ops: []tmcrypto.ProofOp{op.ProofOp()},
+// 			},
+// 		},
+// 	}, nil)
 
-	lc := &lcmock.LightClient{}
-	appHash, _ := hex.DecodeString("5EFD44055350B5CC34DBD26085347A9DBBE44EA192B9286A9FC107F40EA1FAC5")
-	lc.On("VerifyLightBlockAtHeight", context.Background(), int64(2), mock.AnythingOfType("time.Time")).Return(
-		&types.LightBlock{
-			SignedHeader: &types.SignedHeader{
-				Header: &types.Header{AppHash: appHash},
-			},
-		},
-		nil,
-	)
+// 	lc := &lcmock.LightClient{}
+// 	appHash, _ := hex.DecodeString("5EFD44055350B5CC34DBD26085347A9DBBE44EA192B9286A9FC107F40EA1FAC5")
+// 	lc.On("VerifyLightBlockAtHeight", context.Background(), int64(2), mock.AnythingOfType("time.Time")).Return(
+// 		&types.LightBlock{
+// 			SignedHeader: &types.SignedHeader{
+// 				Header: &types.Header{AppHash: appHash},
+// 			},
+// 		},
+// 		nil,
+// 	)
 
-	c := NewClient(next, lc,
-		KeyPathFn(func(_ string, key []byte) (merkle.KeyPath, error) {
-			kp := merkle.KeyPath{}
-			kp = kp.AppendKey(key, merkle.KeyEncodingURL)
-			return kp, nil
-		}))
-	c.RegisterOpDecoder("ics23:iavl", testOpDecoder)
-	res, err := c.ABCIQuery(context.Background(), "/store/accounts/key", key)
-	require.NoError(t, err)
+// 	c := NewClient(next, lc,
+// 		KeyPathFn(func(_ string, key []byte) (merkle.KeyPath, error) {
+// 			kp := merkle.KeyPath{}
+// 			kp = kp.AppendKey(key, merkle.KeyEncodingURL)
+// 			return kp, nil
+// 		}))
+// 	c.RegisterOpDecoder("ics23:iavl", testOpDecoder)
+// 	res, err := c.ABCIQuery(context.Background(), "/store/accounts/key", key)
+// 	require.NoError(t, err)
 
-	assert.NotNil(t, res)
-}
+// 	assert.NotNil(t, res)
+// }
 
 type testOp struct {
 	Spec  *ics23.ProofSpec
