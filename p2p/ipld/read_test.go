41999790f23f8506e874c0d9ad49ffc71154be30
evan-forbes <evan.samuel.forbes@gmail.com>
2021-04-07 22:28:54 -0500

    re adjust timeouts

--- p2p/ipld/read_test.go
+++ p2p/ipld/read_test.go
@@ -305,7 +305,7 @@ func TestRetrieveBlockData(t *testing.T) {
 				t.Fatal(err)
 			}
 
-			retrievalCtx, cancel := context.WithTimeout(background, time.Second*4)
+			retrievalCtx, cancel := context.WithTimeout(background, time.Second*8)
 			defer cancel()
 
 			rblockData, err := RetrieveBlockData(
