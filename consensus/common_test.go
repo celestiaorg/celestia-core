382f3bdcca86b4fea16ffbf811ab51020cd0ef0a
marbar3778 <marbar3778@yahoo.com>
2021-04-12 11:07:45 +0200

    try bump to 600

--- consensus/common_test.go
+++ consensus/common_test.go
@@ -52,7 +52,7 @@ type cleanupFunc func()
 var (
 	config                *cfg.Config // NOTE: must be reset for each _test.go file
 	consensusReplayConfig *cfg.Config
-	ensureTimeout         = time.Millisecond * 450
+	ensureTimeout         = time.Millisecond * 600
 )
 
 func ensureDir(dir string, mode os.FileMode) {
