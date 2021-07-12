daf469ae357c92674630e0b5c958f1ced4d4a8f5
evan-forbes <evan.samuel.forbes@gmail.com>
2021-07-12 06:07:56 -0500

    remove unused tests

--- types/vote_set_test.go
+++ types/vote_set_test.go
@@ -220,34 +220,6 @@ func TestVoteSet_2_3MajorityRedux(t *testing.T) {
 			"there should be no 2/3 majority: last vote added was nil")
 	}
 
-	// // 68th validator voted for a different BlockParts PartSetHeader
-	// {
-	// 	pubKey, err := privValidators[67].GetPubKey()
-	// 	require.NoError(t, err)
-	// 	addr := pubKey.Address()
-	// 	vote := withValidator(voteProto, addr, 67)
-	// 	blockPartsHeader := PartSetHeader{blockPartsTotal, crypto.CRandBytes(32)}
-	// 	_, err = signAddVote(privValidators[67], withBlockPartSetHeader(vote, blockPartsHeader), voteSet)
-	// 	require.NoError(t, err)
-	// 	blockID, _, ok = voteSet.TwoThirdsMajority()
-	// 	assert.False(t, ok || !blockID.IsZero(),
-	// 		"there should be no 2/3 majority: last vote added had different PartSetHeader Hash")
-	// }
-
-	// 69th validator voted for different BlockParts Total
-	// {
-	// 	pubKey, err := privValidators[68].GetPubKey()
-	// 	require.NoError(t, err)
-	// 	addr := pubKey.Address()
-	// 	vote := withValidator(voteProto, addr, 68)
-	// 	blockPartsHeader := PartSetHeader{blockPartsTotal + 1, blockPartSetHeader.Hash}
-	// 	_, err = signAddVote(privValidators[68], withBlockPartSetHeader(vote, blockPartsHeader), voteSet)
-	// 	require.NoError(t, err)
-	// 	blockID, _, ok = voteSet.TwoThirdsMajority()
-	// 	assert.False(t, ok || !blockID.IsZero(),
-	// 		"there should be no 2/3 majority: last vote added had different PartSetHeader Total")
-	// }
-
 	// 70th validator voted for different BlockHash
 	{
 		pubKey, err := privValidators[69].GetPubKey()
