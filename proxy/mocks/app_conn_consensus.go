7eb51b1b8a7a8b6ae0d5ea3683981186c6e38a71
evan-forbes <evan.samuel.forbes@gmail.com>
2021-09-21 15:30:18 -0500

    run make mockery

--- proxy/mocks/app_conn_consensus.go
+++ proxy/mocks/app_conn_consensus.go
@@ -146,13 +146,13 @@ func (_m *AppConnConsensus) InitChainSync(_a0 context.Context, _a1 types.Request
 	return r0, r1
 }
 
-// PreprocessTxsSync provides a mock function with given fields: _a0
-func (_m *AppConnConsensus) PreprocessTxsSync(_a0 types.RequestPreprocessTxs) (*types.ResponsePreprocessTxs, error) {
-	ret := _m.Called(_a0)
+// PreprocessTxsSync provides a mock function with given fields: _a0, _a1
+func (_m *AppConnConsensus) PreprocessTxsSync(_a0 context.Context, _a1 types.RequestPreprocessTxs) (*types.ResponsePreprocessTxs, error) {
+	ret := _m.Called(_a0, _a1)
 
 	var r0 *types.ResponsePreprocessTxs
-	if rf, ok := ret.Get(0).(func(types.RequestPreprocessTxs) *types.ResponsePreprocessTxs); ok {
-		r0 = rf(_a0)
+	if rf, ok := ret.Get(0).(func(context.Context, types.RequestPreprocessTxs) *types.ResponsePreprocessTxs); ok {
+		r0 = rf(_a0, _a1)
 	} else {
 		if ret.Get(0) != nil {
 			r0 = ret.Get(0).(*types.ResponsePreprocessTxs)
@@ -160,8 +160,8 @@ func (_m *AppConnConsensus) PreprocessTxsSync(_a0 types.RequestPreprocessTxs) (*
 	}
 
 	var r1 error
-	if rf, ok := ret.Get(1).(func(types.RequestPreprocessTxs) error); ok {
-		r1 = rf(_a0)
+	if rf, ok := ret.Get(1).(func(context.Context, types.RequestPreprocessTxs) error); ok {
+		r1 = rf(_a0, _a1)
 	} else {
 		r1 = ret.Error(1)
 	}
