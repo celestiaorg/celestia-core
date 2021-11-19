326670126e6556820712a935358632c969750838
mconcat <monoidconcat@gmail.com>
2021-11-19 16:30:01 +0100

    move UNKNOWN response behaviour to reject

--- abci/example/kvstore/persistent_kvstore.go
+++ abci/example/kvstore/persistent_kvstore.go
@@ -178,7 +178,7 @@ func (app *PersistentKVStoreApplication) PrepareProposal(
 func (app *PersistentKVStoreApplication) ProcessProposal(
 	req types.RequestProcessProposal) types.ResponseProcessProposal {
 	for _, tx := range req.Txs {
-		if len(tx) <= 0 {
+		if len(tx) == 0 {
 			return types.ResponseProcessProposal{Result: types.ResponseProcessProposal_REJECT}
 		}
 	}
