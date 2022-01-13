bbdaed366d03cd74ff96337b7d84dcadc0656814
sweexordious <chamirachid1@gmail.com>
2022-01-13 15:58:24 +0100

    removes proto files from celestia-core

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
+# should be executed at the root directory of the project
 set -eo pipefail
+
+git clone https://github.com/celestiaorg/spec
+
+cp -r spec/proto/tendermint proto/tendermint
 
 buf generate --path proto/tendermint
 
-mv ./proto/tendermint/abci/types.pb.go ./abci/types
+find proto/tendermint/ -name "*.proto" -exec rm -rf {} \;
+
+rm -rf spec
