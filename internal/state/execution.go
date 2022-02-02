tendermint/tendermint

abci: Vote Extension 1 (#6646)
* add proto, add boilerplates

* add canonical

* fix tests

* add vote signing test

* Update internal/consensus/msgs_test.go

* modify state execution in progress

* add extension signing

* VoteExtension -> ExtendVote

* apply review

* update data structures

* Add comments

* Apply suggestions from code review

Co-authored-by: Aleksandr Bezobchuk <alexanderbez@users.noreply.github.com>

* *Signed -> *ToSign

* add Vote to RequestExtendVote

* apply reviews

* Apply suggestions from code review

Co-authored-by: Dev Ojha <ValarDragon@users.noreply.github.com>
Co-authored-by: Aleksandr Bezobchuk <alexanderbez@users.noreply.github.com>

* fix typo, modify proto

Co-authored-by: Aleksandr Bezobchuk <alexanderbez@users.noreply.github.com>
Co-authored-by: Dev Ojha <ValarDragon@users.noreply.github.com>

state/execution.go
@@ -265,6 +265,20 @@ func (blockExec *BlockExecutor) ApplyBlock(
 	return state, nil
 }
 
+func (blockExec *BlockExecutor) ExtendVote(vote *types.Vote) (types.VoteExtension, error) {
+  ctx := context.TODO()
+  req := abci.RequestExtendVote{
+    Vote: vote.ToProto(),
+  }
+
+  resp, err := blockExec.proxyApp.ExtendVoteSync(ctx, req)
+  if err != nil {
+    return types.VoteExtension{}, err
+  }
+
+  return types.VoteExtensionFromProto(resp.VoteExtension), nil
+}
+
 // Commit locks the mempool, runs the ABCI Commit message, and updates the
 // mempool.
 // It returns the result of calling abci.Commit (the AppHash) and the height to retain (if any).
@@ -512,7 +526,7 @@ func updateState(
 
 	nextVersion := state.Version
 
-	// NOTE: the AppHash has not been populated.
+	// NOTE: the AppHash and the VoteExtension has not been populated.
 	// It will be filled on state.Save.
 	return State{
 		Version:                          nextVersion,

celestiaorg/celestia-core

internal/state/execution.go
@@ -304,6 +304,20 @@ func (blockExec *BlockExecutor) ApplyBlock(
 	return state, nil
 }
 
+func (blockExec *BlockExecutor) ExtendVote(vote *types.Vote) (types.VoteExtension, error) {
+  ctx := context.TODO()
+  req := abci.RequestExtendVote{
+    Vote: vote.ToProto(),
+  }
+
+  resp, err := blockExec.proxyApp.ExtendVote(ctx, req)
+  if err != nil {
+    return types.VoteExtension{}, err
+  }
+
+  return types.VoteExtensionFromProto(resp.VoteExtension), nil
+}
+
 // Commit locks the mempool, runs the ABCI Commit message, and updates the
 // mempool.
 // It returns the result of calling abci.Commit (the AppHash) and the height to retain (if any).
@@ -541,7 +555,7 @@ func updateState(
 
 	nextVersion := state.Version
 
-	// NOTE: the AppHash has not been populated.
+	// NOTE: the AppHash and the VoteExtension has not been populated.
 	// It will be filled on state.Save.
 	return State{
 		Version:                          nextVersion,