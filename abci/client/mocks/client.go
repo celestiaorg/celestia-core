5173cb8f3a899452c4dbd78a7298b9c8ea8d99f0
evan-forbes <evan.samuel.forbes@gmail.com>
2022-07-24 15:02:47 +0200

    fix tests
    go mod tidy
    
    revert enabling the super linter
    
    try fixing the super linter

--- abci/client/mocks/client.go
+++ abci/client/mocks/client.go
@@ -781,13 +781,13 @@ func (_m *Client) String() string {
 	return r0
 }
 
-type mockConstructorTestingTNewClient interface {
+type NewClientT interface {
 	mock.TestingT
 	Cleanup(func())
 }
 
 // NewClient creates a new instance of Client. It also registers a testing interface on the mock and a cleanup function to assert the mocks expectations.
-func NewClient(t mockConstructorTestingTNewClient) *Client {
+func NewClient(t NewClientT) *Client {
 	mock := &Client{}
 	mock.Mock.Test(t)
 
