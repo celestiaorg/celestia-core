077512f1c698f09f1afa056e251ecd92abbb0da4
Rootul Patel <rootulp@gmail.com>
2022-09-14 17:38:40 -0400

    remove remaining consts that are not used in celestia-core

--- pkg/consts/consts.go
+++ pkg/consts/consts.go
@@ -4,7 +4,6 @@ import (
 	"crypto/sha256"
 
 	"github.com/celestiaorg/nmt/namespace"
-	"github.com/celestiaorg/rsmt2d"
 )
 
 const (
@@ -21,31 +20,9 @@ var (
 
 	// TxNamespaceID is the namespace reserved for transaction data
 	TxNamespaceID = namespace.ID{0, 0, 0, 0, 0, 0, 0, 1}
-	// IntermediateStateRootsNamespaceID is the namespace reserved for
-	// intermediate state root data
-	// TODO(liamsi): code commented out but kept intentionally.
-	// IntermediateStateRootsNamespaceID = namespace.ID{0, 0, 0, 0, 0, 0, 0, 2}
-
-	// EvidenceNamespaceID is the namespace reserved for evidence
-	EvidenceNamespaceID = namespace.ID{0, 0, 0, 0, 0, 0, 0, 3}
-
-	// MaxReservedNamespace is the lexicographically largest namespace that is
-	// reserved for protocol use. It is derived from NAMESPACE_ID_MAX_RESERVED
-	// https://github.com/celestiaorg/celestia-specs/blob/master/src/specs/consensus.md#constants
-	MaxReservedNamespace = namespace.ID{0, 0, 0, 0, 0, 0, 0, 255}
-	// TailPaddingNamespaceID is the namespace ID for tail padding. All data
-	// with this namespace will be ignored
-	TailPaddingNamespaceID = namespace.ID{0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFE}
-	// ParitySharesNamespaceID indicates that share contains erasure data
-	ParitySharesNamespaceID = namespace.ID{0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF}
 
 	// NewBaseHashFunc change accordingly if another hash.Hash should be used as a base hasher in the NMT:
 	NewBaseHashFunc = sha256.New
-
-	// DefaultCodec is the default codec creator used for data erasure
-	// TODO(ismail): for better efficiency and a larger number shares
-	// we should switch to the rsmt2d.LeopardFF16 codec:
-	DefaultCodec = rsmt2d.NewRSGF8Codec
 
 	// DataCommitmentBlocksLimit is the limit to the number of blocks we can generate a data commitment for.
 	DataCommitmentBlocksLimit = 1000
