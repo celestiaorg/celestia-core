62fb5e116c5442d30c7e82e9933c6e4df0cbbd45
Rootul Patel <rootulp@gmail.com>
2022-11-28 15:47:16 -0500

    revert: TestMaxProposalBlockSize to resolve TODO

--- node/node_test.go
+++ node/node_test.go
@@ -370,7 +370,7 @@ func TestMaxProposalBlockSize(t *testing.T) {
 
 	// fill the mempool with one txs just below the maximum size
 	txLength := int(types.MaxDataBytesNoEvidence(maxBytes, 1))
-	tx := tmrand.Bytes(txLength - 4 - 5) // to account for the varint
+	tx := tmrand.Bytes(txLength - 4)
 	err = mempool.CheckTx(tx, nil, mempl.TxInfo{})
 	assert.NoError(t, err)
 
@@ -391,9 +391,7 @@ func TestMaxProposalBlockSize(t *testing.T) {
 
 	pb, err := block.ToProto()
 	require.NoError(t, err)
-	// TODO(ismail): fix this test properly
-	// https://github.com/tendermint/tendermint/issues/77
-	assert.Less(t, int64(pb.Size()), maxBytes*2)
+	assert.Less(t, int64(pb.Size()), maxBytes)
 
 	// check that the part set does not exceed the maximum block size
 	partSet := block.MakePartSet(partSize)
