046ee0c9536874e28bb7df68c614fb64a9cca9f1
Callum Waters <cmwaters19@gmail.com>
2023-01-06 12:51:09 +0100

    blocksync: run process proposal when verifying new blocks (#929)
    Blocksync didn't previously call ProcessProposal. This meant that nodes syncing don't check the Data they receive with the hash in the header. This meant that malicious nodes could fabricate blocks packed with extra unused data or worse they could include transactions that lead to a different app hash causing the syncing node to panic when ApplyBlock errors (because of an app hash mismatch). This commit adds a `ProcessProposal` call in blocksync v0
    
    Co-authored-by: Rootul P <rootulp@gmail.com>

--- blockchain/v0/reactor.go
+++ blockchain/v0/reactor.go
@@ -367,6 +367,21 @@ FOR_LOOP:
 				chainID, firstID, first.Height, second.LastCommit)
 
 			if err == nil {
+				var stateMachineValid bool
+				// Block sync doesn't check that the `Data` in a block is valid.
+				// Since celestia-core can't determine if the `Data` in a block
+				// is valid, the next line asks celestia-app to check if the
+				// block is valid via ProcessProposal. If this step wasn't
+				// performed, a malicious node could fabricate an alternative
+				// set of transactions that would cause a different app hash and
+				// thus cause this node to panic.
+				stateMachineValid, err = bcR.blockExec.ProcessProposal(first)
+				if !stateMachineValid {
+					err = fmt.Errorf("application has rejected syncing block (%X) at height %d", first.Hash(), first.Height)
+				}
+			}
+
+			if err == nil {
 				// validate the block before we persist it
 				err = bcR.blockExec.ValidateBlock(state, first)
 			}
