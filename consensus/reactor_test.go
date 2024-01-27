187d26c36b5c16aa8e2a5ee061b2c7784fbd9a14
Callum Waters <cmwaters19@gmail.com>
2024-01-26 18:38:06 -0600

    fix test I just broke

--- consensus/reactor_test.go
+++ consensus/reactor_test.go
@@ -367,7 +367,7 @@ func TestReactorRecordsVotesAndBlockParts(t *testing.T) {
 	ps := peer.Get(types.PeerStateKey).(*PeerState)
 
 	assert.Equal(t, true, ps.VotesSent() > 0, "number of votes sent should have increased")
-	assert.Equal(t, true, ps.BlockPartsSent(), "should have block")
+	assert.Equal(t, true, ps.BlockPartsSent() > 0, "should have block")
 }
 
 //-------------------------------------------------------------
