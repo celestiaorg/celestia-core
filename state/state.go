e9dc0496f901bb8791cfcb36d2d89feebb675c12
Callum Waters <cmwaters19@gmail.com>
2024-02-13 14:05:00 +0100

    use app version in genesis file if it is set

--- state/state.go
+++ state/state.go
@@ -27,12 +27,14 @@ var (
 // but leaves the Consensus.App version blank.
 // The Consensus.App version will be set during the Handshake, once
 // we hear from the app what protocol version it is running.
-var InitStateVersion = cmtstate.Version{
-	Consensus: cmtversion.Consensus{
-		Block: version.BlockProtocol,
-		App:   0,
-	},
-	Software: version.TMCoreSemVer,
+func InitStateVersion(appVersion uint64) cmtstate.Version {
+	return cmtstate.Version{
+		Consensus: cmtversion.Consensus{
+			Block: version.BlockProtocol,
+			App:   appVersion,
+		},
+		Software: version.TMCoreSemVer,
+	}
 }
 
 //-----------------------------------------------------------------------------
@@ -331,8 +333,13 @@ func MakeGenesisState(genDoc *types.GenesisDoc) (State, error) {
 		nextValidatorSet = types.NewValidatorSet(validators).CopyIncrementProposerPriority(1)
 	}
 
+	appVersion := uint64(0)
+	if genDoc.ConsensusParams != nil {
+		appVersion = genDoc.ConsensusParams.Version.App
+	}
+
 	return State{
-		Version:       InitStateVersion,
+		Version:       InitStateVersion(appVersion),
 		ChainID:       genDoc.ChainID,
 		InitialHeight: genDoc.InitialHeight,
 
