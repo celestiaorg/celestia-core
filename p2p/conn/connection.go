5efe5feef8910bd8f5e4b59bb1c0a346321c98b6
sanaz <sanaztaheri@ieee.org>
2024-02-16 13:11:52 -0800

    some minor addition

--- p2p/conn/connection.go
+++ p2p/conn/connection.go
@@ -688,7 +688,7 @@ FOR_LOOP:
 		switch pkt := packet.Sum.(type) {
 		case *tmp2p.Packet_PacketPing:
 			// TODO: prevent abuse, as they cause flush()'s.
-			// https://github.com/tendermint/tendermint/issues/1190
+			// https://github.com/cometbft/cometbft/issues/1190
 			c.Logger.Debug("Receive Ping")
 			select {
 			case c.pong <- struct{}{}:
