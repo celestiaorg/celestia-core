3498a18b1dee3f22da57866d473d8fa974049354
Rootul Patel <rootulp@gmail.com>
2024-04-04 12:31:35 -0400

    fix: resolve merge conflicts

--- scripts/proto-gen.sh
+++ scripts/proto-gen.sh
@@ -10,11 +10,7 @@ cd "$(git rev-parse --show-toplevel)"
 
 # Run inside Docker to install the correct versions of the required tools
 # without polluting the local system.
-<<<<<<< HEAD
-docker run --rm -i -v "$PWD":/w --workdir=/w golang:1.22-alpine sh <<"EOF"
-=======
 docker run --rm -i -v "$PWD":/w --workdir=/w golang:1.22.2-alpine sh <<"EOF"
->>>>>>> abc516304 (chore: go vuln (#1283))
 apk add git make
 
 go install github.com/bufbuild/buf/cmd/buf
