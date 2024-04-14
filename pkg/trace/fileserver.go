d16a6c0aa65f1342537abb32466982d14343d4bd
evan-forbes <evan.samuel.forbes@gmail.com>
2024-04-14 10:39:28 -0500

    fix: retry pushing to s3 if it fails

--- pkg/trace/fileserver.go
+++ pkg/trace/fileserver.go
@@ -219,11 +219,25 @@ func PushS3(chainID, nodeID string, s3cfg S3Config, f *os.File) error {
 
 	key := fmt.Sprintf("%s/%s/%s", chainID, nodeID, filepath.Base(f.Name()))
 
-	_, err = s3Svc.PutObject(&s3.PutObjectInput{
-		Bucket: aws.String(s3cfg.BucketName),
-		Key:    aws.String(key),
-		Body:   f,
-	})
+	push := func() error {
+		_, err = s3Svc.PutObject(&s3.PutObjectInput{
+			Bucket: aws.String(s3cfg.BucketName),
+			Key:    aws.String(key),
+			Body:   f,
+		})
+		return err
+	}
+
+	err = push()
+	if err != nil {
+		for i := 0; i < 5; i++ {
+			time.Sleep(time.Second * time.Duration(rand.Intn(10)))
+			err = push()
+			if err == nil {
+				continue
+			}
+		}
+	}
 
 	return err
 }
@@ -245,15 +259,9 @@ func (lt *LocalTracer) PushAll() error {
 			return err
 		}
 		defer f.Close()
-		for i := 0; i < 5; i++ {
-			err = PushS3(lt.chainID, lt.nodeID, lt.s3Config, f)
-			if err == nil {
-				break
-			}
-			lt.logger.Error("failed to push table", "table", table, "error", err)
-			time.Sleep(time.Second * time.Duration(rand.Intn(10))) //nolint:gosec
+		if err := PushS3(lt.chainID, lt.nodeID, lt.s3Config, f); err != nil {
+			return err
 		}
-		f.Close()
 	}
 	return nil
 }
@@ -289,7 +297,7 @@ func S3Download(dst, prefix string, cfg S3Config) error {
 
 	err = s3Svc.ListObjectsV2Pages(input, func(page *s3.ListObjectsV2Output, lastPage bool) bool {
 		for _, content := range page.Contents {
-			localFilePath := filepath.Join(dst, strings.TrimPrefix(*content.Key, prefix))
+			localFilePath := filepath.Join(dst, prefix, strings.TrimPrefix(*content.Key, prefix))
 			fmt.Printf("Downloading %s to %s\n", *content.Key, localFilePath)
 
 			// Create the directories in the path
