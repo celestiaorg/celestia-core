5a6903d7b85cc953fc0ac8828dd827e6f9c16c1a
evan-forbes <evan.samuel.forbes@gmail.com>
2024-04-23 15:16:58 -0500

    fix!: update the seek

--- pkg/trace/buffered_file.go
+++ pkg/trace/buffered_file.go
@@ -2,14 +2,26 @@ package trace
 
 import (
 	"bufio"
+	"io"
 	"os"
 	"sync"
+	"sync/atomic"
 )
 
+// bufferedFile is a file that is being written to and read from. It is thread
+// safe, however, when reading from the file, writes will be ignored.
 type bufferedFile struct {
-	mut *sync.RWMutex
+	// reading protects the file from being written to while it is being read
+	// from. This is needed beyond in addition to the mutex so that writes can
+	// be ignored while reading.
+	reading atomic.Bool
+
+	// mut protects the buffered writer.
+	mut *sync.Mutex
+
 	// file is the file that is being written to.
 	file *os.File
+
 	// writer is the buffered writer that is writing to the file.
 	wr *bufio.Writer
 }
@@ -17,40 +29,68 @@ type bufferedFile struct {
 // newbufferedFile creates a new buffered file that writes to the given file.
 func newbufferedFile(file *os.File) *bufferedFile {
 	return &bufferedFile{
-		mut:  &sync.RWMutex{},
-		file: file,
-		wr:   bufio.NewWriter(file),
+		file:    file,
+		wr:      bufio.NewWriter(file),
+		reading: atomic.Bool{},
+		mut:     &sync.Mutex{},
 	}
 }
 
-// Write writes the given bytes to the file.
+// Write writes the given bytes to the file. If the file is currently being read
+// from, the write will be lost.
 func (f *bufferedFile) Write(b []byte) (int, error) {
+	if f.reading.Load() {
+		return 0, nil
+	}
 	f.mut.Lock()
 	defer f.mut.Unlock()
 	return f.wr.Write(b)
 }
 
-// Flush flushes the writer to the file.
-func (f *bufferedFile) Flush() error {
+func (f *bufferedFile) startReading() error {
+	f.reading.Store(true)
 	f.mut.Lock()
 	defer f.mut.Unlock()
-	return f.wr.Flush()
+
+	err := f.wr.Flush()
+	if err != nil {
+		return err
+	}
+
+	_, err = f.file.Seek(0, io.SeekStart)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
-// File returns a new copy of *os.File.
-func (f *bufferedFile) File() (*os.File, error) {
-	err := f.Flush()
+func (f *bufferedFile) stopReading() error {
+	defer f.reading.Store(false)
+	_, err := f.file.Seek(0, io.SeekEnd)
+	return err
+}
+
+// File returns the underlying file with the seek point reset. The caller should
+// not close the file. The caller must call the returned function when they are
+// done reading from the file. This function resets the seek point to where it
+// was being written to.
+func (f *bufferedFile) File() (*os.File, func() error, error) {
+	err := f.startReading()
 	if err != nil {
-		return nil, err
+		return nil, func() error { return nil }, err
 	}
-	f.mut.RLock()
-	defer f.mut.RUnlock()
-	return os.Open(f.file.Name())
+	err = f.wr.Flush()
+	if err != nil {
+		return nil, func() error { return nil }, err
+	}
+
+	return f.file, f.stopReading, nil
 }
 
 // Close closes the file.
 func (f *bufferedFile) Close() error {
-	f.mut.Lock()
-	defer f.mut.Unlock()
+	// set reading to true to prevent writes while closing the file.
+	f.reading.Store(true)
 	return f.file.Close()
 }
