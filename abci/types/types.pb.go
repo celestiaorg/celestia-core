7e97bb0f2b8747e4ad144589295c078268ce1a89
Rootul Patel <rootulp@gmail.com>
2024-06-13 15:53:56 -0400

    revert: newline changes

--- abci/types/types.pb.go
+++ abci/types/types.pb.go
@@ -1408,6 +1408,7 @@ func (m *RequestProcessProposal) GetBlockData() *types1.Data {
 
 type Response struct {
 	// Types that are valid to be assigned to Value:
+	//
 	//	*Response_Exception
 	//	*Response_Echo
 	//	*Response_Flush
