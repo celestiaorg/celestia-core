a7012440ecaa8c58a2f978c8005d9cff13f16ae6
Callum Waters <cmwaters19@gmail.com>
2024-07-31 16:41:51 +0200

    Update state/execution.go
    Co-authored-by: Rootul P <rootulp@gmail.com>

--- state/execution.go
+++ state/execution.go
@@ -147,7 +147,6 @@ func (blockExec *BlockExecutor) CreateProposalBlock(
 	}
 	rawNewData := preparedProposal.GetBlockData()
 
-	// don't count the last tx in rpp.Txs which is data root back from app
 	rejectedTxs := len(rawNewData.Txs) - len(txs)
 	if rejectedTxs > 0 {
 		blockExec.metrics.RejectedTransactions.Add(float64(rejectedTxs))
