854c3cd99c785cc86d696dee20427c8e8a36dd87
sweexordious <chamirachid1@gmail.com>
2024-11-29 00:15:58 +0100

    chore: add millisecond sleep in mock reactor

--- p2p/load/mock_reactor.go
+++ p2p/load/mock_reactor.go
@@ -326,6 +326,7 @@ func (mr *MockReactor) FloodChannel(id p2p.ID, d time.Duration, chIDs ...byte) {
 		go func(d time.Duration, chID byte) {
 			start := time.Now()
 			for time.Since(start) < d {
+				time.Sleep(time.Millisecond)
 				success := mr.SendBytes(id, chID, mr.size.Load())
 				if success {
 					mr.metrics.mtx.Lock()
