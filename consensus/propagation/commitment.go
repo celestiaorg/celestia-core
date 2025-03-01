728c4bb9fb117f85a21b21e56dccf2be1e4c29f3
sweexordious <chamirachid1@gmail.com>
2025-03-01 17:18:06 +0400

    feat: support catchup

--- consensus/propagation/commitment.go
+++ consensus/propagation/commitment.go
@@ -73,7 +73,11 @@ func chunkToPartMetaData(chunk *bits.BitArray, partSet *types.PartSet) []*propag
 // time a proposal is received from a peer or when a proposal is created. If the
 // proposal is new, it will be stored and broadcast to the relevant peers.
 func (blockProp *Reactor) handleCompactBlock(cb *proptypes.CompactBlock, peer p2p.ID) {
-	// todo(evan) handle catchup if someone sends us a proposal that is much further in the future.
+	if cb.Proposal.Height > blockProp.currentHeight+1 || cb.Proposal.Round > blockProp.currentRound+1 {
+		// catchup on missing heights/rounds by requesting the missing parts from all connected peers.
+		go blockProp.requestMissingBlocks(cb.Proposal.Height, cb.Proposal.Round)
+	}
+
 	added, _, _ := blockProp.AddProposal(cb)
 
 	// todo: add catchup logic here by checking for gaps
