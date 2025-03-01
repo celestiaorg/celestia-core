1cc6fd5ce94de0605663a2ea659f36a431f09084
sweexordious <chamirachid1@gmail.com>
2025-03-01 17:18:06 +0400

    feat: support catchup

--- consensus/propagation/catchup.go
+++ consensus/propagation/catchup.go
@@ -1,45 +1,54 @@
 package propagation
 
 import (
+	proptypes "github.com/tendermint/tendermint/consensus/propagation/types"
 	"github.com/tendermint/tendermint/p2p"
-	"github.com/tendermint/tendermint/types"
 )
 
-// sendPsh sends the partset header to the provided peer.
-// This method will:
-// - get the proposal with the provided height and round
-// - get the patset header from the block meta?
-// - send the partset header to the provided peer
-// TODO rename this Psh to something less Psh
+// sendPsh sends the part set header to the provided peer.
+// TODO check if we still need this?
 func (blockProp *Reactor) sendPsh(peer p2p.ID, height int64, round int32) bool {
 	return true
 }
 
-// HandleValidBlock is called when the node finds a peer with a valid block. If this
-// node doesn't have a block, it asks the sender for the portions that it
-// doesn't have.
-// This method will:
-// - get the provided peer from the peer state
-// - get the proposal referenced by the provided height and round
-// - if it has the proposal:
-//   - if the proposal is complete return
-//   - otherwise, create a new bit array with the size of the partset header total and fill it with true indices
-//   - broadcast the haves of that block
-//
-// - send the wants of all the block parts to the peer that sent it to us
-// - set the requests
-// - request all the previous blocks if any are missing
-func (blockProp *Reactor) HandleValidBlock(peer p2p.ID, height int64, round int32, psh types.PartSetHeader, exitEarly bool) {
-}
+// requestMissingBlocks is called when a node is catching up and needs to
+// request all previous blocks from the compact block source peer.
+func (blockProp *Reactor) requestMissingBlocks(targetHeight int64, targetRound int32) {
+	// if we're missing heights, we will be requesting them from the connected peers
+	for height := blockProp.currentHeight + 1; height < targetHeight; height++ {
+		heightProposal, partSet, found := blockProp.GetProposal(height, -1)
+		if !found {
+			blockProp.Logger.Error("couldn't find proposal", "height", height, "round", -1)
+			continue
+		}
+		if partSet.BitArray() == nil {
+			panic("partSet is nil when getting proposal!")
+		}
+		missingParts := partSet.BitArray().Not()
+		wantPart := &proptypes.WantParts{
+			Parts:  missingParts,
+			Height: height,
+			Round:  heightProposal.Round,
+		}
+		blockProp.broadcastWants(wantPart, blockProp.self)
+	}
 
-// requestAllPreviousBlocks is called when a node is catching up and needs to
-// request all previous blocks from a peer.
-// What this method will do:
-// - get the peer from state
-// - get the reactor latest height
-// - send want parts for all the necessary blocks between [reactor.latestHeight, height)
-// while setting the want's round to a value < 0.
-//
-//nolint:unused
-func (blockProp *Reactor) requestAllPreviousBlocks(peer p2p.ID, height int64) {
+	// if we're missing rounds for the current height, request them from all the peers
+	for round := int32(0); round < targetRound; round++ {
+		_, partSet, found := blockProp.GetProposal(targetHeight, round)
+		if !found {
+			blockProp.Logger.Error("couldn't find proposal", "height", targetHeight, "round", round)
+			continue
+		}
+		if partSet.BitArray() == nil {
+			panic("partSet is nil when getting proposal!")
+		}
+		missingParts := partSet.BitArray().Not()
+		wantPart := &proptypes.WantParts{
+			Parts:  missingParts,
+			Height: targetHeight,
+			Round:  round,
+		}
+		blockProp.broadcastWants(wantPart, blockProp.self)
+	}
 }
