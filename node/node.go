3e341b21b5ad7a1caea6b4287f94a440d1c8c57b
CHAMI Rachid <chamirachid1@gmail.com>
2025-03-03 20:16:41 +0100

    feat: save the compact block when proposing and send it when connecting (#1645)
    ## Description
    
    Saves the compact block locally when proposing a block.
    
    #### PR checklist
    
    - [ ] Tests written/updated
    - [ ] Changelog entry added in `.changelog` (we use
    [unclog](https://github.com/informalsystems/unclog) to manage our
    changelog)
    - [ ] Updated relevant documentation (`docs/` or `spec/`) and code
    comments

--- node/node.go
+++ node/node.go
@@ -1006,6 +1006,7 @@ func makeNodeInfo(
 			mempl.MempoolChannel,
 			evidence.EvidenceChannel,
 			statesync.SnapshotChannel, statesync.ChunkChannel,
+			propagation.DataChannel, propagation.WantChannel,
 		},
 		Moniker: config.Moniker,
 		Other: p2p.DefaultNodeInfoOther{
