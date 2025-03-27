23f7a57bec4a05d03685f53b6eb8a859ae115b3f
Marko <marbar3778@yahoo.com>
2025-03-27 13:07:08 +0100

    chore: increase grpc message size to allow for BIG BLONKS (#1694)
    ## Description
    
    This will need to be released and added into v3 as part of the v4
    upgrade. It will not effect running nodes today
    
    #### PR checklist
    
    - [ ] Tests written/updated
    - [ ] Changelog entry added in `.changelog` (we use
    [unclog](https://github.com/informalsystems/unclog) to manage our
    changelog)
    - [ ] Updated relevant documentation (`docs/` or `spec/`) and code
    comments

--- abci/server/grpc_server.go
+++ abci/server/grpc_server.go
@@ -10,6 +10,8 @@ import (
 	"github.com/tendermint/tendermint/libs/service"
 )
 
+const mebibyte = 1024 * 1024
+
 type GRPCServer struct {
 	service.BaseService
 
@@ -43,7 +45,9 @@ func (s *GRPCServer) OnStart() error {
 	}
 
 	s.listener = ln
-	s.server = grpc.NewServer()
+	s.server = grpc.NewServer(
+		grpc.MaxRecvMsgSize(mebibyte * 75),
+	)
 	types.RegisterABCIApplicationServer(s.server, s.app)
 
 	s.Logger.Info("Listening", "proto", s.proto, "addr", s.addr)
