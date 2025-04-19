51623122f6a37638db21b382abb810d6332a7b9c
sweexordious <chamirachid1@gmail.com>
2025-04-19 16:26:08 +0100

    chore: fix

--- consensus/reactor.go
+++ consensus/reactor.go
@@ -132,7 +132,7 @@ func (conR *Reactor) SwitchToConsensus(state sm.State, skipWAL bool) {
 		// NOTE: The line below causes broadcastNewRoundStepRoutine() to broadcast a
 		// NewRoundStepMessage.
 		conR.conS.updateToState(state)
-		conR.propagator.SetConsensusHeight(height)
+		conR.propagator.SetConsensusHeight(state.LastBlockHeight)
 	}()
 
 	conR.mtx.Lock()
