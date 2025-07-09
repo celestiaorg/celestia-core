f840574a744e05efd549dc07ec1a2a69b57ef4e5
sweexordious <chamirachid1@gmail.com>
2025-07-09 14:49:59 +0100

    chore: fix test

--- consensus/propagation/types/types_test.go
+++ consensus/propagation/types/types_test.go
@@ -234,7 +234,7 @@ func TestWantParts_ValidateBasic(t *testing.T) {
 	}{
 		{
 			"valid want parts",
-			WantParts{Height: 1, Round: 1, Parts: &bits.BitArray{}},
+			WantParts{Height: 1, Round: 1, Parts: &bits.BitArray{}, MissingPartsCount: 10},
 			false,
 		},
 		{
