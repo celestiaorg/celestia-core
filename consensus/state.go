136280790e6837264a35e728102b598c9986d8e5
sweexordious <chamirachid1@gmail.com>
2025-07-25 10:11:55 +0400

    feat: delay pre-emptive block building

--- consensus/state.go
+++ consensus/state.go
@@ -1709,6 +1709,9 @@ func (cs *State) buildNextBlock() {
 	case <-cs.nextBlock:
 	default:
 	}
+	if cs.config.TimeoutCommit.Seconds() > 2 {
+		time.Sleep(time.Duration(cs.config.TimeoutCommit.Nanoseconds() - 1_500_000_000))
+	}
 	block, blockParts, err := cs.createProposalBlock(context.TODO())
 	if err != nil {
 		cs.Logger.Error("unable to create proposal block", "error", err)
