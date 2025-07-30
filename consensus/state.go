61558b8faf881649f30d94d5b97b851090ac2c63
CHAMI Rachid <chamirachid1@gmail.com>
2025-07-30 11:20:31 +0400

    feat: delay pre-emptive block building (#2255)
    Closes https://github.com/celestiaorg/celestia-core/issues/2235

--- consensus/state.go
+++ consensus/state.go
@@ -1702,13 +1702,20 @@ func (cs *State) enterPrecommitWait(height int64, round int32) {
 	cs.scheduleTimeout(cs.config.Precommit(round), height, round, cstypes.RoundStepPrecommitWait)
 }
 
-// buildNextBlock creates the next block pre-amptively if we're the proposer.
+// blockBuildingTime the time it takes to build a new 32 mb block and a safety cushion.
+const blockBuildingTime = 1800 * time.Millisecond
+
+// buildNextBlock creates the next block pre-emptively if we're the proposer.
 func (cs *State) buildNextBlock() {
 	select {
 	// flush the next block channel to ensure only the relevant block is there.
 	case <-cs.nextBlock:
 	default:
 	}
+
+	// delay pre-emptive block building until the end of the timeout commit
+	time.Sleep(cs.config.TimeoutCommit - blockBuildingTime)
+
 	block, blockParts, err := cs.createProposalBlock(context.TODO())
 	if err != nil {
 		cs.Logger.Error("unable to create proposal block", "error", err)
