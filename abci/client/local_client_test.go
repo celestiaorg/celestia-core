a2311ab13c21c52a45082bae3b7092c3ee79730b
Rootul Patel <rootulp@gmail.com>
2025-08-19 14:29:54 +0200

    test: async

--- abci/client/local_client_test.go
+++ abci/client/local_client_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/cometbft/cometbft/abci/types"
 	cmtsync "github.com/cometbft/cometbft/libs/sync"
+	"github.com/stretchr/testify/require"
 )
 
 func TestNewLocalClient(t *testing.T) {
@@ -16,7 +17,24 @@ func TestNewLocalClient(t *testing.T) {
 
 		mutex.Lock()
 		// If CheckTx called Lock on the mutex, the next line would cause a deadlock.
-		client.CheckTx(context.Background(), &types.RequestCheckTx{})
+		_, err := client.CheckTx(context.Background(), &types.RequestCheckTx{})
+		require.NoError(t, err)
 		mutex.Unlock()
 	})
+	t.Run("CheckTxAsync should not lock mutex", func(t *testing.T) {
+		mutex := new(cmtsync.Mutex)
+		app := types.NewBaseApplication()
+		client := NewLocalClient(mutex, app)
+		client.SetResponseCallback(mockCallback())
+
+		mutex.Lock()
+		// If CheckTxAsync called Lock on the mutex, the next line would cause a deadlock.
+		_, err := client.CheckTxAsync(context.Background(), &types.RequestCheckTx{})
+		require.NoError(t, err)
+		mutex.Unlock()
+	})
+}
+
+func mockCallback() func(*types.Request, *types.Response) {
+	return func(req *types.Request, res *types.Response) {}
 }
