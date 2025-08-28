b8243d2af85c81feff4b90c18ca0a56b6aba74ac
sweexordious <chamirachid1@gmail.com>
2025-08-28 10:56:27 +0400

    chore: remove magic number

--- test/e2e/node/main.go
+++ test/e2e/node/main.go
@@ -33,6 +33,8 @@ import (
 	e2e "github.com/cometbft/cometbft/test/e2e/pkg"
 )
 
+const defaultE2EDelayedCommitTimeout = 20 * time.Millisecond
+
 var logger = log.NewTMLogger(log.NewSyncWriter(os.Stdout))
 
 func init() {
@@ -132,7 +134,7 @@ func startNode(cfg *Config) error {
 	}
 
 	cmtcfg.Instrumentation.TraceType = "local"
-	cmtcfg.Consensus.DelayedPrecommitTimeout = 20 * time.Millisecond
+	cmtcfg.Consensus.DelayedPrecommitTimeout = defaultE2EDelayedCommitTimeout
 
 	var clientCreator proxy.ClientCreator
 	if cfg.Protocol == string(e2e.ProtocolBuiltinConnSync) {
@@ -163,7 +165,7 @@ func startLightClient(cfg *Config) error {
 	if err != nil {
 		return err
 	}
-	cmtcfg.Consensus.DelayedPrecommitTimeout = 1000 * time.Millisecond
+	cmtcfg.Consensus.DelayedPrecommitTimeout = defaultE2EDelayedCommitTimeout
 
 	dbContext := &config.DBContext{ID: "light", Config: cmtcfg}
 	lightDB, err := config.DefaultDBProvider(dbContext)
