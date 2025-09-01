37feaa0eaf6bf789b77438741260fd94b1834b97
CHAMI Rachid <chamirachid1@gmail.com>
2025-09-01 20:38:08 +0400

    feat: support delayed precommits (#2382)
    Closes:
    https://github.com/celestiaorg/celestia-core/issues/2381#issuecomment-3229416390
    
    ---------
    
    Co-authored-by: evan-forbes <evan.samuel.forbes@gmail.com>

--- config/config.go
+++ config/config.go
@@ -1044,6 +1044,9 @@ type ConsensusConfig struct {
 	TimeoutPrecommit time.Duration `mapstructure:"timeout_precommit"`
 	// How much the timeout_precommit increases with each round
 	TimeoutPrecommitDelta time.Duration `mapstructure:"timeout_precommit_delta"`
+	// DelayedPrecommitTimeout how much time to wait before entering precommit starting
+	// from the commit time
+	DelayedPrecommitTimeout time.Duration // TODO dynamically change using the app
 	// How long we wait after committing a block, before starting on the new
 	// height (this gives us a chance to receive some more precommits, even
 	// though we already have +2/3).
@@ -1079,6 +1082,7 @@ func DefaultConsensusConfig() *ConsensusConfig {
 		TimeoutPrecommit:            1000 * time.Millisecond,
 		TimeoutPrecommitDelta:       500 * time.Millisecond,
 		TimeoutCommit:               1000 * time.Millisecond,
+		DelayedPrecommitTimeout:     1000 * time.Millisecond,
 		SkipTimeoutCommit:           false,
 		CreateEmptyBlocks:           true,
 		CreateEmptyBlocksInterval:   0 * time.Second,
@@ -1099,6 +1103,7 @@ func TestConsensusConfig() *ConsensusConfig {
 	cfg.TimeoutPrevoteDelta = 1 * time.Millisecond
 	cfg.TimeoutPrecommit = 10 * time.Millisecond
 	cfg.TimeoutPrecommitDelta = 1 * time.Millisecond
+	cfg.DelayedPrecommitTimeout = 0
 	// NOTE: when modifying, make sure to update time_iota_ms (testGenesisFmt) in toml.go
 	cfg.TimeoutCommit = 10 * time.Millisecond
 	cfg.SkipTimeoutCommit = true
