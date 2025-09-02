e2e6347ae8308758ec1c468d4d8f3e70ead6124f
sweexordious <chamirachid1@gmail.com>
2025-09-02 12:46:33 +0400

    Revert "chore: call ParallelProofsFromByteSlices from cb.Proofs"
    This reverts commit 1897300510a941163c9bbcd1fe1c34a26bd4a236.

--- consensus/propagation/types/types.go
+++ consensus/propagation/types/types.go
@@ -208,14 +208,14 @@ func (c *CompactBlock) Proofs() ([]*merkle.Proof, error) {
 
 	c.proofsCache = make([]*merkle.Proof, 0, len(c.PartsHashes))
 
-	root, proofs := merkle.ParallelProofsFromByteSlices(c.PartsHashes[:total])
+	root, proofs := merkle.ProofsFromLeafHashes(c.PartsHashes[:total])
 	c.proofsCache = append(c.proofsCache, proofs...)
 
 	if !bytes.Equal(root, c.Proposal.BlockID.PartSetHeader.Hash) {
 		return c.proofsCache, fmt.Errorf("incorrect PartsHash: original root")
 	}
 
-	parityRoot, eproofs := merkle.ParallelProofsFromByteSlices(c.PartsHashes[total:])
+	parityRoot, eproofs := merkle.ProofsFromLeafHashes(c.PartsHashes[total:])
 	c.proofsCache = append(c.proofsCache, eproofs...)
 
 	if !bytes.Equal(c.BpHash, parityRoot) {
