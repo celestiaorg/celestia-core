39ab4e4a67d2acdf77ad67088490d767849c38e3
evan-forbes <evan.samuel.forbes@gmail.com>
2025-09-01 19:40:43 -0500

    feat: store proposals with invalid signatures to the data/debug

--- types/utils.go
+++ types/utils.go
@@ -1,6 +1,7 @@
 package types
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -59,6 +60,42 @@ func SaveBlockToFile(dir string, filename string, block *Block) error {
 
 	if _, err := file.Write(blockBZ); err != nil {
 		return fmt.Errorf("failed to write block to file: %w", err)
+	}
+
+	return nil
+}
+
+// SaveInvalidProposalWithMetadata saves an invalid proposal with metadata to a JSON file for debugging
+func SaveInvalidProposalWithMetadata(dir string, filename string, proposal *Proposal, metadata map[string]interface{}) error {
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return fmt.Errorf("failed to create directory: %w", err)
+	}
+
+	filePath := filepath.Join(dir, filename)
+
+	file, err := os.Create(filePath)
+	if err != nil {
+		return fmt.Errorf("failed to create file: %w", err)
+	}
+	defer file.Close()
+
+	// Create a structure that combines proposal data and metadata
+	proposalData := struct {
+		Metadata map[string]interface{} `json:"metadata"`
+		Proposal *Proposal              `json:"proposal"`
+	}{
+		Metadata: metadata,
+		Proposal: proposal,
+	}
+
+	// Marshal to pretty JSON for readability
+	proposalBZ, err := json.MarshalIndent(proposalData, "", "  ")
+	if err != nil {
+		return fmt.Errorf("failed to marshal proposal data to JSON: %w", err)
+	}
+
+	if _, err := file.Write(proposalBZ); err != nil {
+		return fmt.Errorf("failed to write proposal data to file: %w", err)
 	}
 
 	return nil
