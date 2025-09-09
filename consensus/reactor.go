ab0a75fde0b35ba67b5a62be9c6c78065d431d74
sweexordious <chamirachid1@gmail.com>
2025-09-09 10:54:10 +0400

    Revert "chore: use delayed commit for sleep"
    This reverts commit bf0e9a4711e90279630e642152a396bd9ff8bd8d.

--- consensus/reactor.go
+++ consensus/reactor.go
@@ -670,13 +670,16 @@ func (conR *Reactor) getRoundState() *cstypes.RoundState {
 func (conR *Reactor) gossipDataRoutine(peer p2p.Peer, ps *PeerState) {
 	logger := conR.Logger.With("peer", peer)
 	isGossipDataEnabled := conR.IsGossipDataEnabled()
+	conR.conS.mtx.Lock()
 	sleepDuration := conR.conS.config.PeerGossipSleepDuration
 	if !isGossipDataEnabled {
-		sleepDuration = conR.conS.state.Timeouts.DelayedPrecommitTimeout
+		// TODO switch to delayed
+		sleepDuration = conR.conS.state.TimeoutCommit
 		if sleepDuration == 0 {
 			sleepDuration = conR.conS.config.DelayedPrecommitTimeout
 		}
 	}
+	conR.conS.mtx.Unlock()
 OUTER_LOOP:
 	for {
 		// Manage disconnects from self or peer.
