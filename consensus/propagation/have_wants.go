71b034c068e5708eeb8811a8ee38d9dd6c8597fa
CHAMI Rachid <chamirachid1@gmail.com>
2025-09-23 18:13:31 +0400

    chore: not show the parts when returning error (#2478)
    Please review the [CONTRIBUTING.md](../CONTRIBUTING.md) file for
    detailed contribution guidelines.

--- consensus/propagation/have_wants.go
+++ consensus/propagation/have_wants.go
@@ -52,7 +52,7 @@ func (blockProp *Reactor) handleHaves(peer p2p.ID, haves *proptypes.HaveParts) {
 	}
 	err := haves.ValidatePartHashes(cb.PartsHashes)
 	if err != nil {
-		blockProp.Logger.Error("received invalid have part", "height", haves.Height, "round", haves.Round, "parts", haves.Parts, "err", err)
+		blockProp.Logger.Error("received invalid have part", "height", haves.Height, "round", haves.Round, "err", err)
 		blockProp.Switch.StopPeerForError(p.peer, err, blockProp.String())
 		return
 	}
