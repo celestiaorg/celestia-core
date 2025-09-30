197d7c55b32991ed6a861c5236e757bfdb3021c6
CHAMI Rachid <chamirachid1@gmail.com>
2025-09-30 20:21:24 +0400

    feat: allow disabling the legacy block prop and artificially reduce block size (#2512)
    Closes https://github.com/celestiaorg/celestia-core/issues/2236 and
    https://github.com/celestiaorg/celestia-core/issues/2238
    
    I won't backport this PR to main, given main disables by default the
    legacy block prop. But I will create another PR that introduces this
    change along with the one that added the `disable_block_prop` config.

--- test/e2e/pkg/testnet.go
+++ test/e2e/pkg/testnet.go
@@ -139,6 +139,7 @@ type Node struct {
 	PyroscopeProfileTypes []string
 
 	DisablePropagationReactor bool
+	EnableLegacyBlockProp     bool
 }
 
 // LoadTestnet loads a testnet from a manifest file, using the filename to
@@ -266,6 +267,7 @@ func NewTestnetFromManifest(manifest Manifest, file string, ifd InfrastructureDa
 			PyroscopeProfileTypes: ifd.PyroscopeProfileTypes,
 
 			DisablePropagationReactor: nodeManifest.DisablePropagationReactor,
+			EnableLegacyBlockProp:     nodeManifest.EnableLegacyBlockProp,
 		}
 		if node.StartAt == testnet.InitialHeight {
 			node.StartAt = 0 // normalize to 0 for initial nodes, since code expects this
@@ -384,7 +386,7 @@ func (t Testnet) Validate() error {
 	if len(t.Nodes) == 0 {
 		return errors.New("network has no nodes")
 	}
-	if t.BlockMaxBytes > types.MaxBlockSizeBytes {
+	if t.BlockMaxBytes > int64(types.MaxBlockSizeBytes) {
 		return fmt.Errorf("value of BlockMaxBytes cannot be higher than %d", types.MaxBlockSizeBytes)
 	}
 	if t.VoteExtensionsUpdateHeight < -1 {
--- types/params.go
+++ types/params.go
@@ -12,14 +12,12 @@ import (
 )
 
 const (
-	// MaxBlockSizeBytes is the maximum permitted size of the blocks.
-	MaxBlockSizeBytes = 128 * 1024 * 1024
+	DefaultMaxBlockSizeBytes = 128 * 1024 * 1024
+	// ReducedMaxBlockSizeBytes will be used when disabling the propagation reactor in emergency situations
+	ReducedMaxBlockSizeBytes = 8 * 1024 * 1024
 
 	// BlockPartSizeBytes is the size of one block part.
 	BlockPartSizeBytes uint32 = 65536 // 64kB
-
-	// MaxBlockPartsCount is the maximum number of block parts.
-	MaxBlockPartsCount = (MaxBlockSizeBytes / BlockPartSizeBytes) + 1
 
 	ABCIPubKeyTypeEd25519   = ed25519.KeyType
 	ABCIPubKeyTypeSecp256k1 = secp256k1.KeyType
@@ -29,6 +27,14 @@ var ABCIPubKeyTypesToNames = map[string]string{
 	ABCIPubKeyTypeEd25519:   ed25519.PubKeyName,
 	ABCIPubKeyTypeSecp256k1: secp256k1.PubKeyName,
 }
+
+var (
+	// MaxBlockSizeBytes is the maximum permitted size of the blocks.
+	MaxBlockSizeBytes = DefaultMaxBlockSizeBytes
+
+	// MaxBlockPartsCount is the maximum number of block parts.
+	MaxBlockPartsCount = (uint32(MaxBlockSizeBytes) / BlockPartSizeBytes) + 1
+)
 
 // ConsensusParams contains consensus critical parameters that determine the
 // validity of blocks.
@@ -151,7 +157,7 @@ func (params ConsensusParams) ValidateBasic() error {
 
 			params.Block.MaxBytes)
 	}
-	if params.Block.MaxBytes > MaxBlockSizeBytes {
+	if params.Block.MaxBytes > int64(MaxBlockSizeBytes) {
 		return fmt.Errorf("block.MaxBytes is too big. %d > %d",
 			params.Block.MaxBytes, MaxBlockSizeBytes)
 	}
