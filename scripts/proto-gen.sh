c3381354d979374c5bde993fcfbc77828645ae6e
Nina Barbakadze <barbakadzeninaa@gmail.com>
2025-10-15 14:34:14 +0200

    chore: revert unnecessary change

--- scripts/proto-gen.sh
+++ scripts/proto-gen.sh
@@ -10,10 +10,23 @@ cd "$(git rev-parse --show-toplevel)"
 
 # Run inside Docker to install the correct versions of the required tools
 # without polluting the local system.
-docker run --rm -i -v "$PWD":/w --workdir=/w golang:1.20-alpine sh <<"EOF"
+docker run --rm -i -v "$PWD":/w --workdir=/w golang:1.24-alpine sh <<"EOF"
+set -e
 apk add git make
 
-go install github.com/bufbuild/buf/cmd/buf
+go install github.com/bufbuild/buf/cmd/buf@latest
 go install github.com/cosmos/gogoproto/protoc-gen-gogofaster@latest
-make proto-gen
+
+# Add Go bin to PATH so buf can be found
+export PATH="$PATH:/root/go/bin"
+
+# Generate protobuf files
+echo "Generating Protobuf files"
+buf generate
+
+
+# Move generated files to their final locations
+# (paths are relative to /w which is the repo root mounted by Docker)
+mv ./proto/tendermint/abci/types.pb.go ./abci/types/
+cp ./proto/tendermint/rpc/grpc/types.pb.go ./rpc/grpc
 EOF
