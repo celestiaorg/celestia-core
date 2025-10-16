e1ab8ecd173adee058285b74d960a52964b9d8ac
CHAMI Rachid <chamirachid1@gmail.com>
2025-10-16 17:14:06 +0400

    chore(libs): add message stats table (#2575)
    Adds the message stats table that I always add to measure times. It's
    not outputing any traces because they're too much. But I prefer to leave
    it always here so that if anyone wants to use it, they can just call:
    
    ```
    schema.WriteMessageStats(cs.traceClient, "consensus", "proposeBlock", time.Since(start).Nanoseconds(), "")
    ```

--- libs/trace/schema/schema.go
+++ libs/trace/schema/schema.go
@@ -17,6 +17,7 @@ func AllTables() []string {
 	tables = append(tables, P2PTables()...)
 	tables = append(tables, ABCITable)
 	tables = append(tables, RecoveryTables()...)
+	tables = append(tables, MessageStatsTables()...)
 	return tables
 }
 
