32159de6f0862e0805edf5af39df3fb705972dd3
sweexordious <chamirachid1@gmail.com>
2025-10-24 18:38:40 +0400

    chore: use parallel

--- consensus/tests/test_test.go
+++ consensus/tests/test_test.go
@@ -18,7 +18,7 @@ func TestName(t *testing.T) {
 		data = append(data, cmtrand.Bytes(int(types.BlockPartSizeBytes)))
 	}
 	s := time.Now()
-	root, proofs := merkle.ProofsFromByteSlices(data)
+	root, proofs := merkle.ParallelProofsFromLeafHashes(data)
 	fmt.Println("proofs: ", time.Since(s))
 	protoProof := proofs[2000].ToProto()
 	fmt.Println(4000 * protoProof.Size())
