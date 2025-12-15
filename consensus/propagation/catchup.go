70dec030fa518a604dbfd958602d4f1d4058ab16
evan-forbes <evan.samuel.forbes@gmail.com>
2025-12-14 23:11:31 -0600

    fix: request from peers blindly during catchup

--- consensus/propagation/catchup.go
+++ consensus/propagation/catchup.go
@@ -39,9 +39,6 @@ func (blockProp *Reactor) retryWants() {
 		peers = shuffle(peers)
 
 		for _, peer := range peers {
-			if peer.consensusPeerState.GetHeight() < height {
-				continue
-			}
 			mc := missing.Copy()
 
 			reqs, has := peer.GetRequests(height, round)
